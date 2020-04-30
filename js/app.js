e52cc12899abbea18e98a2a1a82bcfe3214004d1
Trevor Stubbs <stubbste@gmail.com>
2020-04-30 15:36:01 -0700

    add functions

--- js/app.js
+++ js/app.js
@@ -1,8 +1,9 @@
 'use strict';
 
+// Score counter
 var counter = 0;
 
-// geting the users name
+// getting the users name
 
 var userName = prompt('Howdy, what is your name?');
 
@@ -13,86 +14,92 @@ alert('Welcome ' + userName + ' to a page all about me! Let us see what you can 
 
 
 // guess if I have 4 cats
+function catAmount(){
+  var howManyCats = prompt(userName + ' do you think I have 4 cats?').toLowerCase();
 
-var howManyCats = prompt(userName + ' do you think I have 4 cats?').toLowerCase();
-
-if(howManyCats.toLowerCase() === 'yes' || howManyCats.toLowerCase() === 'y'){
-  alert('YES! ' + userName + ' more cats the better!');
-  // console.log('correct');
-  counter = counter + 1;
-} else if (howManyCats.toLowerCase() === 'no' || howManyCats.toLowerCase() === 'n'){
-  alert(userName + ' I know I do not always have cats falling off of me but I do in fact have 4 cats.');
-  // console.log('incorrect');
-} else {
-  prompt(userName + ' please answer with yes or no');
+  if(howManyCats.toLowerCase() === 'yes' || howManyCats.toLowerCase() === 'y'){
+    alert('YES! ' + userName + ' more cats the better!');
+    // console.log('correct');
+    counter = counter + 1;
+  } else if (howManyCats.toLowerCase() === 'no' || howManyCats.toLowerCase() === 'n'){
+    alert(userName + ' I know I do not always have cats falling off of me but I do in fact have 4 cats.');
+    // console.log('incorrect');
+  } else {
+    prompt(userName + ' please answer with yes or no');
+  }
 }
 
+
 // guess if I'm always eating
-
-var alwaysEating = prompt('Do you think Im constantly eating in class while coding?');
-
-if(alwaysEating.toLowerCase() === 'yes' || alwaysEating.toLowerCase() === 'y'){
-  alert('That is correct ' + userName + ' coding needs brain food, snacks all every day!');
-  // console.log('correct');
-  counter = counter + 1;
-} else if (alwaysEating.toLowerCase() === 'no' || alwaysEating.toLowerCase() === 'n'){
-  alert('That is very kind of you ' + userName + ' but I am always eating and probably eating right now.');
-  // console.log('incorrect');
-} else{
-  prompt(userName + ' please answer with yes or no');
+function foodMonster(){
+  var alwaysEating = prompt('Do you think Im constantly eating in class while coding?');
+  
+  if(alwaysEating.toLowerCase() === 'yes' || alwaysEating.toLowerCase() === 'y'){
+    alert('That is correct ' + userName + ' coding needs brain food, snacks all every day!');
+    // console.log('correct');
+    counter = counter + 1;
+  } else if (alwaysEating.toLowerCase() === 'no' || alwaysEating.toLowerCase() === 'n'){
+    alert('That is very kind of you ' + userName + ' but I am always eating and probably eating right now.');
+    // console.log('incorrect');
+  } else{
+    prompt(userName + ' please answer with yes or no');
+  }
 }
 
 // guess if I have braces
+function bracesFace(){
+  var braces = prompt('Do I have braces?');
 
-var braces = prompt('Do I have braces?');
-
-if(braces.toLowerCase() === 'yes' || braces.toLowerCase() === 'y'){
-  alert('That is correct ' + userName + ' I am 32 and have braces as an adult!');
-  // console.log('correct');
-  counter = counter + 1;
-} else if (braces.toLowerCase() === 'no' || braces.toLowerCase() === 'n'){
-  alert('Saddly ' + userName + ' that is not correct, they are painful.');
-  // console.log('incorrect');
-} else{
-  prompt(userName + ' please answer with yes or no');
+  if(braces.toLowerCase() === 'yes' || braces.toLowerCase() === 'y'){
+    alert('That is correct ' + userName + ' I am 32 and have braces as an adult!');
+    // console.log('correct');
+    counter = counter + 1;
+  } else if (braces.toLowerCase() === 'no' || braces.toLowerCase() === 'n'){
+    alert('Saddly ' + userName + ' that is not correct, they are painful.');
+    // console.log('incorrect');
+  } else{
+    prompt(userName + ' please answer with yes or no');
+  }
 }
 
-
-
-
-
 // guess if I have skydived
+function planeJumper(){
+  var skydive = prompt('Do you think I have gone skydiving?');
 
-
-var skydive = prompt('Do you think I have gone skydiving?');
-
-if(skydive.toLowerCase() === 'yes' || skydive.toLowerCase() === 'y'){
-  alert('That is correct ' + userName + ', I went skydiving when I was 20!');
-  // console.log('correct');
-  counter = counter + 1;
-} else if (skydive.toLowerCase() === 'no' || skydive.toLowerCase() === 'n'){
-  alert('My mom wishes that was the answer ' + userName + ' but nope I decided to skydive the day of when I took a friend to their appointment!');
-  // console.log('incorrect');
-} else{
-  prompt(userName + ' please answer with yes or no');
+  if(skydive.toLowerCase() === 'yes' || skydive.toLowerCase() === 'y'){
+    alert('That is correct ' + userName + ', I went skydiving when I was 20!');
+    // console.log('correct');
+    counter = counter + 1;
+  } else if (skydive.toLowerCase() === 'no' || skydive.toLowerCase() === 'n'){
+    alert('My mom wishes that was the answer ' + userName + ' but nope I decided to skydive the day of when I took a friend to their appointment!');
+    // console.log('incorrect');
+  } else{
+    prompt(userName + ' please answer with yes or no');
+  }
 }
 
 // guess if I watch the venture bros
+function teamVenture(){
+  var venture = prompt('Have I watched The Venture Bros. series multiple times?');
 
-
-var venture = prompt('Have I watched The Venture Bros. series multiple times?');
-
-if(venture.toLowerCase() === 'yes' || venture.toLowerCase() === 'y'){
-  alert('That is correct ' + userName + ', this cartoon is more than just a cartoon you should really check it out! GO TEAM VENTURE!!!');
-  // console.log('correct');
-  counter = counter + 1;
-} else if (venture.toLowerCase() === 'no' || venture.toLowerCase() === 'n'){
-  alert('That is very much untrue ' + userName + ', but now hopefully I\'ve peaked your interest in checking out this rad show with a million pop culture nerdy easter eggs!');
-  // console.log('incorrect');
-} else{
-  prompt(userName + ' please answer with yes or no');
+  if(venture.toLowerCase() === 'yes' || venture.toLowerCase() === 'y'){
+    alert('That is correct ' + userName + ', this cartoon is more than just a cartoon you should really check it out! GO TEAM VENTURE!!!');
+    // console.log('correct');
+    counter = counter + 1;
+  } else if (venture.toLowerCase() === 'no' || venture.toLowerCase() === 'n'){
+    alert('That is very much untrue ' + userName + ', but now hopefully I\'ve peaked your interest in checking out this rad show with a million pop culture nerdy easter eggs!');
+    // console.log('incorrect');
+  } else{
+    prompt(userName + ' please answer with yes or no');
+  }
 }
 
+//Call the yes or no questions
+catAmount();
+foodMonster();
+bracesFace();
+planeJumper();
+teamVenture();
 
 // question 6 number guessing game
 
@@ -150,13 +157,9 @@ for(var j = 0; j < 6; j++){ // loop through the number of guesses
 
 }
 
+
+
 alert('I\'m sure you are very curious so here are all of the names of my creatures, Lucipurr, Ethel, Spaceghost, and Harry Winsten!');
 
-
-
-
-
-
 alert(userName + ' you got ' + counter + ' out of 7 in my guessing game!');
-
 
