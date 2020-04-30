663185e2574391b5e1c25e74ebb7d0e8df9cb437
Trevor Stubbs <stubbste@gmail.com>
2020-04-30 16:09:17 -0700

    fix little things

--- js/app.js
+++ js/app.js
@@ -1,27 +1,28 @@
 'use strict';
 
-// Score counter
-var counter = 0;
+// Score scoreCounter
+var scoreCounter = 0;
 
 // getting the users name
-
 var userName = prompt('Howdy, what is your name?');
 
 // personalized welcome to page
-
 alert('Welcome ' + userName + ' to a page all about me! Let us see what you can guess about me, good luck!');
 // console.log('Hello ' + userName + ' welcome to my guessing game!');
 
+// Global Array for the yes and no answers
+var yesNoArray = ['yes', 'y' , 'no' , 'n'];
+//                  0     1     2      3
 
 // guess if I have 4 cats
 function catAmount(){
   var howManyCats = prompt(userName + ' do you think I have 4 cats?').toLowerCase();
 
-  if(howManyCats.toLowerCase() === 'yes' || howManyCats.toLowerCase() === 'y'){
+  if(howManyCats.toLowerCase() === yesNoArray[0] || howManyCats.toLowerCase() === yesNoArray[1]){
     alert('YES! ' + userName + ' more cats the better!');
     // console.log('correct');
-    counter = counter + 1;
-  } else if (howManyCats.toLowerCase() === 'no' || howManyCats.toLowerCase() === 'n'){
+    scoreCounter++;
+  } else if (howManyCats.toLowerCase() === yesNoArray[3] || howManyCats.toLowerCase() === yesNoArray[4]){
     alert(userName + ' I know I do not always have cats falling off of me but I do in fact have 4 cats.');
     // console.log('incorrect');
   } else {
@@ -29,16 +30,15 @@ function catAmount(){
   }
 }
 
-
 // guess if I'm always eating
 function foodMonster(){
   var alwaysEating = prompt('Do you think Im constantly eating in class while coding?');
-  
-  if(alwaysEating.toLowerCase() === 'yes' || alwaysEating.toLowerCase() === 'y'){
+
+  if(alwaysEating.toLowerCase() === yesNoArray[0] || alwaysEating.toLowerCase() === yesNoArray[1]){
     alert('That is correct ' + userName + ' coding needs brain food, snacks all every day!');
     // console.log('correct');
-    counter = counter + 1;
-  } else if (alwaysEating.toLowerCase() === 'no' || alwaysEating.toLowerCase() === 'n'){
+    scoreCounter++;
+  } else if (alwaysEating.toLowerCase() === yesNoArray[2] || alwaysEating.toLowerCase() === yesNoArray[3]){
     alert('That is very kind of you ' + userName + ' but I am always eating and probably eating right now.');
     // console.log('incorrect');
   } else{
@@ -50,11 +50,11 @@ function foodMonster(){
 function bracesFace(){
   var braces = prompt('Do I have braces?');
 
-  if(braces.toLowerCase() === 'yes' || braces.toLowerCase() === 'y'){
+  if(braces.toLowerCase() === yesNoArray[0] || braces.toLowerCase() === yesNoArray[1]){
     alert('That is correct ' + userName + ' I am 32 and have braces as an adult!');
     // console.log('correct');
-    counter = counter + 1;
-  } else if (braces.toLowerCase() === 'no' || braces.toLowerCase() === 'n'){
+    scoreCounter++;
+  } else if (braces.toLowerCase() === yesNoArray[2] || braces.toLowerCase() === yesNoArray[3]){
     alert('Saddly ' + userName + ' that is not correct, they are painful.');
     // console.log('incorrect');
   } else{
@@ -66,11 +66,11 @@ function bracesFace(){
 function planeJumper(){
   var skydive = prompt('Do you think I have gone skydiving?');
 
-  if(skydive.toLowerCase() === 'yes' || skydive.toLowerCase() === 'y'){
+  if(skydive.toLowerCase() === yesNoArray[0] || skydive.toLowerCase() === yesNoArray[1]){
     alert('That is correct ' + userName + ', I went skydiving when I was 20!');
     // console.log('correct');
-    counter = counter + 1;
-  } else if (skydive.toLowerCase() === 'no' || skydive.toLowerCase() === 'n'){
+    scoreCounter++;
+  } else if (skydive.toLowerCase() === yesNoArray[2] || skydive.toLowerCase() === yesNoArray[3]){
     alert('My mom wishes that was the answer ' + userName + ' but nope I decided to skydive the day of when I took a friend to their appointment!');
     // console.log('incorrect');
   } else{
@@ -82,11 +82,11 @@ function planeJumper(){
 function teamVenture(){
   var venture = prompt('Have I watched The Venture Bros. series multiple times?');
 
-  if(venture.toLowerCase() === 'yes' || venture.toLowerCase() === 'y'){
+  if(venture.toLowerCase() === yesNoArray[0] || venture.toLowerCase() === yesNoArray[1]){
     alert('That is correct ' + userName + ', this cartoon is more than just a cartoon you should really check it out! GO TEAM VENTURE!!!');
     // console.log('correct');
-    counter = counter + 1;
-  } else if (venture.toLowerCase() === 'no' || venture.toLowerCase() === 'n'){
+    scoreCounter++;
+  } else if (venture.toLowerCase() === yesNoArray[2] || venture.toLowerCase() === yesNoArray[3]){
     alert('That is very much untrue ' + userName + ', but now hopefully I\'ve peaked your interest in checking out this rad show with a million pop culture nerdy easter eggs!');
     // console.log('incorrect');
   } else{
@@ -105,7 +105,7 @@ function matteGame(){
     if(matteNumber === matte){
       alert('Dang ' + userName + ' you are good!');
       // console.log('correct');
-      counter = counter + 1;
+      scoreCounter++;
       break;
     } else if (matte > matteNumber) {
       alert('Sorry, your guess is too high!');
@@ -134,7 +134,7 @@ function crazyCatLady(){
         alert('You got it! That is one of my creatures!');
         guessedCorrect = true;
         // console.log('correct');
-        counter = counter + 1;
+        scoreCounter++;
         break;//will only break us out of the inner for loop
       }
     }
@@ -161,5 +161,5 @@ crazyCatLady();
 
 alert('I\'m sure you are very curious so here are all of the names of my creatures, Lucipurr, Ethel, Spaceghost, and Harry Winsten!');
 
-alert(userName + ' you got ' + counter + ' out of 7 in my guessing game!');
+alert(userName + ' you got ' + scoreCounter + ' out of 7 in my guessing game!');
 
